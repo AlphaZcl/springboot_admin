6fa77cfaa815fcee4c7eb499513a2d0d6952e93d
AlphaZcl <zclys06@163.com>
2021-10-27 11:48:39 +0800

    登录功能

---
+++ src/main/java/top/zhuchl/admin/springboot_admin/controller/IndexController.java
@@ -0,0 +1,48 @@
+package top.zhuchl.admin.springboot_admin.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import top.zhuchl.admin.springboot_admin.pojo.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+/**
+ * @Author AlphaZcl
+ * @Date 2021/10/26
+ **/
+@Controller
+@Slf4j
+public class IndexController {
+
+    @GetMapping(value={"loginPage","/"})
+    public String loginPage(){
+        log.info("登录页面");
+        return "login";
+    }
+
+    @RequestMapping ("signIn")
+    public String login(User user,HttpSession session){
+        log.info("登录信息认证:{}",user);
+        String url = "login";
+        if(user != null && StringUtils.hasLength(user.getUserName()) && StringUtils.hasLength(user.getPassword())){
+            url = "redirect:mainPage";
+            session.setAttribute("user",user);
+        }
+        return url;
+    }
+
+    @GetMapping("mainPage")
+    public String mainPage(@SessionAttribute(required = false) User user){
+        log.info("进入主页User:{}",user);
+        String url = "login";
+        if(user != null && StringUtils.hasLength(user.getUserName()) && StringUtils.hasLength(user.getPassword())){
+            url = "main";
+        }
+        return url;
+    }
+
+}
---
+++ src/main/java/top/zhuchl/admin/springboot_admin/pojo/User.java
@@ -0,0 +1,17 @@
+package top.zhuchl.admin.springboot_admin.pojo;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @Author AlphaZcl
+ * @Date 2021/10/26
+ **/
+@Data
+@ToString
+public class User {
+
+    private String userName;
+
+    private String password;
+}
