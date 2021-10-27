7356158cb35a4a52a299ae2eb2b2a64916a8348c
AlphaZcl <zclys06@163.com>
2021-10-27 17:09:34 +0800

    删除多余导包

--- src/main/java/top/zhuchl/admin/springboot_admin/controller/IndexController.java
+++ src/main/java/top/zhuchl/admin/springboot_admin/controller/IndexController.java
@@ -3,12 +3,12 @@ package top.zhuchl.admin.springboot_admin.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import top.zhuchl.admin.springboot_admin.pojo.User;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.Map;
 
 /**
  * @Author AlphaZcl
